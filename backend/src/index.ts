ed791121006b4989701292ce495fda7a7fcb5749
HOTHEAD01TH <hothead01th@gmail.com>
2025-01-30 21:13:59 +0530

    completed backend and deployed zod validation on npm

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,91 +1,16 @@
 import { Hono } from 'hono';
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
-import { decode, jwt, sign, verify } from 'hono/jwt'
+import { userRouter } from './routes/user';
+import { blogRouter } from './routes/blog';
 
 const app = new Hono<{
 	Bindings: {
 		DATABASE_URL: string;
-		JWT_SECRET: string
+		JWT_SECRET: string;
 	}
 }>();
 
-// app.use('/api/v1/blog/*', async (c, next) => {
-// 	const header = c.req.header('authorization') || "";
-// 	const token = header.split(" ")[1];
-// 	const response = await verify(token , c.env.JWT_SECRET)
-// 	if (response.id) {
-// 		next()
-// 	} else {
-// 		c.status(403)
-// 		return c.json({ error: "Unauthorized" })
-// 	}
-	
-//    })
-
-
-
-app.post('/api/v1/user/signup', async (c) => {
-	const prisma = new PrismaClient({
-		datasourceUrl: c.env.DATABASE_URL,
-	 }).$extends(withAccelerate())
-
-      const body = await c.req.json()
-
-const user = await prisma.user.create({
-	data: {
-		username: body.username,
-		password: body.password,
-	}
-  })
-
-  const token = await sign({ id: user.id }, c.env.JWT_SECRET)
-	return c.json({
-	 jwt: token})
-})
-
-
-app.post('/api/v1/user/signin', async (c) => {
-	const prisma = new PrismaClient({
-		datasourceUrl: c.env?.DATABASE_URL	,
-	}).$extends(withAccelerate());
-
-	const body = await c.req.json();
-	const user = await prisma.user.findUnique({
-		where: {
-			username: body.username,
-			password: body.password
-		}
-	});
-
-	if (!user) {
-		c.status(403);
-		return c.json({ error: "user not found" });
-	}
-
-	const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
-	return c.json({ jwt });
-})
-
-app.post('/api/v1/blog', (c) => {
-
-	return c.text('signin route')
-})
-
-app.put('/api/v1/blog', (c) => {
-	return c.text('signin route')
-})
-
-app.get('/api/v1/blog/:id', (c) => {
-	const id = c.req.param('id')
-	console.log(id);
-	return c.text('get blog route')
-})
-
-
-
-
-
+app.route("/api/v1/user/", userRouter);
+app.route("/api/v1/blog", blogRouter);
 
 
 export default app;
