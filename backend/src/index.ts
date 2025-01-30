c819982ec070961d21adf4f21287bae1cd3d8f9f
HOTHEAD01TH <hothead01th@gmail.com>
2025-01-30 17:43:42 +0530

    migration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,14 +5,27 @@ import { decode, jwt, sign, verify } from 'hono/jwt'
 
 const app = new Hono<{
 	Bindings: {
-		DATABASE_URL: string
+		DATABASE_URL: string;
 		JWT_SECRET: string
 	}
 }>();
 
+// app.use('/api/v1/blog/*', async (c, next) => {
+// 	const header = c.req.header('authorization') || "";
+// 	const token = header.split(" ")[1];
+// 	const response = await verify(token , c.env.JWT_SECRET)
+// 	if (response.id) {
+// 		next()
+// 	} else {
+// 		c.status(403)
+// 		return c.json({ error: "Unauthorized" })
+// 	}
+	
+//    })
 
 
-app.post('/api/v1/signup', async (c) => {
+
+app.post('/api/v1/user/signup', async (c) => {
 	const prisma = new PrismaClient({
 		datasourceUrl: c.env.DATABASE_URL,
 	 }).$extends(withAccelerate())
@@ -21,17 +34,18 @@ app.post('/api/v1/signup', async (c) => {
 
 const user = await prisma.user.create({
 	data: {
-		email: body.email,
+		username: body.username,
 		password: body.password,
 	}
   })
+
   const token = await sign({ id: user.id }, c.env.JWT_SECRET)
 	return c.json({
 	 jwt: token})
 })
 
 
-app.post('/api/v1/signin', async (c) => {
+app.post('/api/v1/user/signin', async (c) => {
 	const prisma = new PrismaClient({
 		datasourceUrl: c.env?.DATABASE_URL	,
 	}).$extends(withAccelerate());
@@ -39,7 +53,8 @@ app.post('/api/v1/signin', async (c) => {
 	const body = await c.req.json();
 	const user = await prisma.user.findUnique({
 		where: {
-			email: body.email
+			username: body.username,
+			password: body.password
 		}
 	});
 
@@ -52,13 +67,6 @@ app.post('/api/v1/signin', async (c) => {
 	return c.json({ jwt });
 })
 
-
-app.get('/api/v1/blog/:id', (c) => {
-	const id = c.req.param('id')
-	console.log(id);
-	return c.text('get blog route')
-})
-
 app.post('/api/v1/blog', (c) => {
 
 	return c.text('signin route')
@@ -67,6 +75,16 @@ app.post('/api/v1/blog', (c) => {
 app.put('/api/v1/blog', (c) => {
 	return c.text('signin route')
 })
+
+app.get('/api/v1/blog/:id', (c) => {
+	const id = c.req.param('id')
+	console.log(id);
+	return c.text('get blog route')
+})
+
+
+
+
 
 
 
